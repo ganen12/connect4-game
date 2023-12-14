60764d497c048cd99911d7fcc54ffc3b8197b357
Kanaka <135590646+ganen12@users.noreply.github.com>
2023-12-14 13:13:32 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,50 +3,63 @@
 #include <cctype>
 #include <cstdlib>
 #include <ctime>
+#include <iomanip> 
 
 using namespace std;
 
 const char PLAYER1 = 'x';
 const char PLAYER2 = 'o';
-const int ROW = 6;
-const int COLUMN = 7;
 
 char board[6][7];
 
 void displayMainMenu();
 void resetBoard();
 void displayBoard();
-void player1Turn();
-void player2Turn();
 int checkFreeSpaces();
 char checkWinner();
 void printWinner(char);
+void placeChecker(char player);
 
 
 int main() {
-	char winner = ' ';
+	char winner;
 	char playAgain = ' ';
 
 	do {
+		winner = ' ';
 		resetBoard();
 
 		while (winner == ' ' && checkFreeSpaces() != 0) {
+
+			system("cls");
 			displayBoard();
 
-			player1Turn();
+			placeChecker(PLAYER1);
 			if (checkWinner() != ' ' || checkFreeSpaces() == 0) {
+				winner = PLAYER1;
 				break;
 			}
 
-			player2Turn();
+			system("cls");
+			displayBoard();
+
+			placeChecker(PLAYER2);
 			if (checkWinner() != ' ' || checkFreeSpaces() == 0) {
+				winner = PLAYER2;
 				break;
 			}
-
 		}
+
+		system("cls");
+		displayBoard();
+		printWinner(winner);
+
+		cout << "\n\tPlay again? (y/n):";
+		cin >> playAgain;
 		
 	} while (playAgain == 'Y' || playAgain == 'y');
 	
+	cout << "\n\tTerima kasih sudah bermain Connect 4!\n";
 	
 	return 0;
 }
@@ -63,19 +76,31 @@ void resetBoard() {
 }
 
 void displayBoard() {
-	cout << " " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << " | " << board[0][3] << " | " << board[0][4] << " | " << board[0][5] << " | " << board[0][6] << " ";
-	cout << "\n---|---|---|---|---|---|---\n";
-	cout << " " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << " | " << board[1][3] << " | " << board[1][4] << " | " << board[1][5] << " | " << board[1][6] << " ";
-	cout << "\n---|---|---|---|---|---|---\n";
-	cout << " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << " | " << board[2][3] << " | " << board[2][4] << " | " << board[2][5] << " | " << board[2][6] << " ";
-	cout << "\n---|---|---|---|---|---|---\n";
-	cout << " " << board[3][0] << " | " << board[3][1] << " | " << board[3][2] << " | " << board[3][3] << " | " << board[3][4] << " | " << board[3][5] << " | " << board[3][6] << " ";
-	cout << "\n---|---|---|---|---|---|---\n";
-	cout << " " << board[4][0] << " | " << board[4][1] << " | " << board[4][2] << " | " << board[4][3] << " | " << board[4][4] << " | " << board[4][5] << " | " << board[4][6] << " ";
-	cout << "\n---|---|---|---|---|---|---\n";
-	cout << " " << board[5][0] << " | " << board[5][1] << " | " << board[5][2] << " | " << board[5][3] << " | " << board[5][4] << " | " << board[5][5] << " | " << board[5][6] << " ";
-	cout << "\n";
 
+	// keren bet jirr
+	cout << "  _____                            _     ______               \n";
+	cout << " / ____|                          | |   |  ____|              \n";
+	cout << "| |     ___  _ __  _ __   ___  ___| |_  | |__ ___  _   _ _ __ \n";
+	cout << "| |    / _ \\| '_ \\| '_ \\ / _ \\/ __| __| |  __/ _ \\| | | | '__|\n";
+	cout << "| |___| (_) | | | | | | |  __/ (__| |_  | | | (_) | |_| | |   \n";
+	cout << " \\_____\\___/|_| |_|_| |_|\\___|\\___|\\__| |_|  \\___/ \\__,_|_|   \n";
+	cout << "                                                               \n";
+	cout << "                                                              \n";
+	// gila	
+
+	cout << "\t      " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << " | " << board[0][3] << " | " << board[0][4] << " | " << board[0][5] << " | " << board[0][6] << " ";
+	cout << "\n\t     ---|---|---|---|---|---|---\n";
+	cout << "\t      " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << " | " << board[1][3] << " | " << board[1][4] << " | " << board[1][5] << " | " << board[1][6] << " ";
+	cout << "\n\t     ---|---|---|---|---|---|---\n";
+	cout << "\t      " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << " | " << board[2][3] << " | " << board[2][4] << " | " << board[2][5] << " | " << board[2][6] << " ";
+	cout << "\n\t     ---|---|---|---|---|---|---\n";
+	cout << "\t      " << board[3][0] << " | " << board[3][1] << " | " << board[3][2] << " | " << board[3][3] << " | " << board[3][4] << " | " << board[3][5] << " | " << board[3][6] << " ";
+	cout << "\n\t     ---|---|---|---|---|---|---\n";
+	cout << "\t      " << board[4][0] << " | " << board[4][1] << " | " << board[4][2] << " | " << board[4][3] << " | " << board[4][4] << " | " << board[4][5] << " | " << board[4][6] << " ";
+	cout << "\n\t     ---|---|---|---|---|---|---\n";
+	cout << "\t      " << board[5][0] << " | " << board[5][1] << " | " << board[5][2] << " | " << board[5][3] << " | " << board[5][4] << " | " << board[5][5] << " | " << board[5][6] << " ";
+	cout << "\n\t      1   2   3   4   5   6   7 \n";
+	cout << "\n";
 }
 
 int checkFreeSpaces() {
@@ -92,23 +117,34 @@ int checkFreeSpaces() {
 	return freeSpaces;
 }
 
-// nantinya player turn akan menjadi satu fungsi
-void player1Turn() {
-	
+void placeChecker(char player) {
+
 	int colPlacement;
-	int rowPlacement = 5;
 
+	switch (player) {
+		case 'x':
+			cout << "\n    --> PLAYER 1'S TURN (X) <--        PLAYER 2\n\n";
+			break;
+		case 'o':
+			cout << "\n        PLAYER 1                   --> PLAYER 2'S TURN (O) <--\n\n";
+			break;
+	}
+
+	// 
 	do {
-		
-		cout << "Pilih kolom (1-7):";
-		cin >> colPlacement;
+		int rowPlacement = 5;
 
-		// mungkin tidak digunakan, karena setiap playerTurn() dipanggil, rowPlacement = 5.
-		// tanpa memerhatikan jika ada placement di bawah input colPlacement, rowPlacement akan selalu dikurangi 1 sampai ada tempat kosong 
-		for (int i = 0; i < 6; i++) {
-			if (board[5][colPlacement - 1] == ' ') {
-				rowPlacement = 5;
-				break;
+		// Validasi input
+		while (true) {
+			cout << "\tPilih kolom (1-7):\n\t-> ";
+			cin >> colPlacement;
+			if (colPlacement >= 1 && colPlacement <= 7) {
+				break; // Valid input, exit loop
+			}
+			else {
+				cout << "\n\tInvalid input. Masukkan angka 1-7.\n";
+				cin.clear(); // Clear error flag
+				cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear input buffer
 			}
 		}
 
@@ -117,54 +153,16 @@ void player1Turn() {
 				rowPlacement--;
 			}
 			else if (board[rowPlacement][colPlacement - 1] == ' ') {
-				board[rowPlacement][colPlacement - 1] = PLAYER1;
+				board[rowPlacement][colPlacement - 1] = player;
 				break;
-			} 
+			}
 		}
 
-		if (rowPlacement < 0) {
-			cout << "No more space available, try another column!"<<endl;
-		}
-		else if (board[rowPlacement][colPlacement - 1] == PLAYER1) {
+		if (board[rowPlacement][colPlacement - 1] == player) {
 			break;
 		}
 
-	} while (true);
-
-}
-
-void player2Turn() {
-
-	int colPlacement;
-	int rowPlacement = 5;
-	do {
-
-		cout << "Pilih kolom (1-7):";
-		cin >> colPlacement;
-
-		for (int i = 0; i < 6; i++) {
-			if (board[5][colPlacement - 1] == ' ') {
-				rowPlacement = 5;
-				break;
-			}
-		}
-
-		while (rowPlacement >= 0) {
-			if (board[rowPlacement][colPlacement - 1] != ' ') {
-				rowPlacement--;
-			}
-			else if (board[rowPlacement][colPlacement - 1] == ' ') {
-				board[rowPlacement][colPlacement - 1] = PLAYER2;
-				break;
-			}
-		}
-
-		if (rowPlacement < 0) {
-			cout << "No more space available, try again" << endl;
-		}
-		else if (board[rowPlacement][colPlacement - 1] == PLAYER2) {
-			break;
-		}
+		cout << "\n\tTidak ada lagi ruang yang tersedia, coba kolom lain!" << endl;
 
 	} while (true);
 
@@ -172,4 +170,65 @@ void player2Turn() {
 
 char checkWinner() {
 
+	// Check secara horizontal
+	for (int i = 0; i < 6; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			if (board[i][j] == board[i][j + 1] &&
+				board[i][j] == board[i][j + 2] &&
+				board[i][j] == board[i][j + 3] &&
+				board[i][j] != ' ') {
+				return board[i][j];
+			}
+		}
+	}
+
+	// Check secara vertikal
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 7; ++j) {
+			if (board[i][j] == board[i + 1][j] &&
+				board[i][j] == board[i + 2][j] &&
+				board[i][j] == board[i + 3][j] &&
+				board[i][j] != ' ') {
+				return board[i][j];
+			}
+		}
+	}
+
+	// Check secara diagonal (dari kiri atas ke kanan bawah)
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			if (board[i][j] == board[i + 1][j + 1] &&
+				board[i][j] == board[i + 2][j + 2] &&
+				board[i][j] == board[i + 3][j + 3] &&
+				board[i][j] != ' ') {
+				return board[i][j];
+			}
+		}
+	}
+
+	// Check secara diagonal (dari kanan atas ke kiri bawah)
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 3; j < 7; ++j) {
+			if (board[i][j] == board[i + 1][j - 1] &&
+				board[i][j] == board[i + 2][j - 2] &&
+				board[i][j] == board[i + 3][j - 3] &&
+				board[i][j] != ' ') {
+				return board[i][j];
+			}
+		}
+	}
+
+	return ' '; // jika belum ada checker yang 4 kali berurutan, return char kosong
+}
+
+void printWinner(char winner) {
+	if (winner == PLAYER1) {
+		cout << "\tPlayer 1 (X) wins!" << endl;
+	}
+	else if (winner == PLAYER2) {
+		cout << "\tPlayer 2 (O) wins!" << endl;
+	}
+	else {
+		cout << "\tIt's a draw!" << endl;
+	}
 }
